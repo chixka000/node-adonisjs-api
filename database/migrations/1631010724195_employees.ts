bd957f3477712e86ba98688c6114b87631db44cc
rycangwapo <ryancanseco12@gmail.com>
2021-09-07 18:54:29 +0800

    add Employee Table

---
+++ database/migrations/1631010724195_employees.ts
@@ -0,0 +1,36 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class EmployeeProfiles extends BaseSchema {
+  protected tableName = 'employees'
+
+  public async up () {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id').primary()
+      table.string('first_name').notNullable()
+      table.string('last_name').notNullable()
+      table.string('birthdate').notNullable()
+      table.string('gender').notNullable()
+      table.string('marital_status').notNullable()
+      table.string('department').notNullable()
+      table.string('position').notNullable()
+      table.string('date_hired').notNullable()
+      table.string('employment_status').notNullable()
+      table.bigInteger('contact_number').notNullable()
+      table.string('email')
+      table.string('address')
+      table.string('city')
+      table.string('province')
+      table.string('nationality')
+
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down () {
+    this.schema.dropTable(this.tableName)
+  }
+}
